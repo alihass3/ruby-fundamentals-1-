1f0afa089a9121f85966a35703761966ae79f1cb
user <user@users-MacBook-Air.local>
2017-06-26 15:17:48 -0400

    Completed excercise 4

--- excercise3.rb
+++ excercise3.rb
@@ -1,3 +1,11 @@
 puts "What is your name?"
-user_name=gets
+user_name=gets.chomp
 puts "Hello, #{user_name}"
+puts "Age?"
+user_age=gets.chomp
+puts "Hey #{user_name},you were born in #{2017-(user_age.to_i)}"
+
+# lets do a bit of .times
+3.times do
+  puts "Print"
+end
