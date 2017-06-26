f4e48f3b3a8bee3d7197c75c4a44301129d9f51c
user <user@users-MacBook-Air.local>
2017-06-26 14:09:38 -0400

    Prompting user

---
+++ excercise2.rb
@@ -0,0 +1,8 @@
+#solution for 1: 10 percent of total maybe lol
+puts "A typical tip for a 55 dollar bill would be #{0.1*55}"
+#umm kinda tricky lets see
+puts "Result is #{"lol"+5.to_s}"
+# will use interpolation here
+puts "The result of multiplication is #{45628*7839}"
+# put this shit in and find out
+puts "Result comes out to be #{(10 < 20 && 30 < 20) || !(10 == 11)}"
